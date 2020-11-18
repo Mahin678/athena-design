9a53c9a2b5a35e5e325b557f150d13f439f7bb8d
Lancer Abir <anjan.o24@gmail.com>
2020-11-18 23:49:02 +0600

    new work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    return <div className = "App" > hello i am lancer abir < /div>
+    return <div className = "App" > new work </div>;
 }
 
 export default App;
