16e5ea1171151f09529923487f240f568bdd0e9e
Lancer Abir <anjan.o24@gmail.com>
2020-11-18 23:58:38 +0600

    nnn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-    return <div className = "App" > new work </div>;
+    return <div className = "App" > <h2> new pull</h2></div>;
 }
 
 export default App;
