f850e45f1c06e24b5874f4fe4a908ce36d484ff2
Lancer Abir <anjan.o24@gmail.com>
2020-11-20 12:27:29 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+//** Import React Router */
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+//** Import Pages Components */
+import Home from './components/Pages/Home/Home';
+import About from './components/Pages/About/About';
+import Services from './components/Pages/Services/Services';
+import Pricing from './components/Pages/Pricing/Pricing';
+import OurTeam from './components/Pages/OurTeam/OurTeam';
+import NotFound from './components/Pages/NotFound';
 
 function App() {
-	return <div className="App"> new work everything</div>;
+	return (
+        <BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/about" component={About} />
+				<Route exact path="/services" component={Services} />
+				<Route exact path="/pricing" component={Pricing} />
+				<Route exact path="/ourTeam" component={OurTeam} />
+				<Route path="*" component={NotFound} />
+			</Switch>
+		</BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+//** Import Template Parts */
+import Header from "../../templateParts/Header/Header";
+import Footer from "../../templateParts/Footer/Footer";
+
+const Home = () => {
+   return (
+      <div>
+         <Header></Header>
+         <Footer></Footer>
+      </div>
+   );
+};
+
+export default Home;
